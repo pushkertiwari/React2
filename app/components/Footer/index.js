2129671c0c09d5e5acc86b5c65ea6d27bbec7f30
pushkertiwari <pushker@revinfotech.com>
2016-12-19 16:47:27 +0530

    pushker pc setup

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -13,7 +13,7 @@ function Footer() {
         <FormattedMessage {...messages.licenseMessage} />
       </section>
       <section>
-        <LocaleToggle />
+        
       </section>
       <section>
         <FormattedMessage
